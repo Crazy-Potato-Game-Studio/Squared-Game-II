5002265a8aa07bac7178466a3500185b846fd3cd
mikolaj <mikolaj.cwen123@gmail.com>
2024-03-09 15:52:45 +0100

    09.03.2024

--- Assets/Scripts/Mechanisms/ColorChangerScript.cs
+++ Assets/Scripts/Mechanisms/ColorChangerScript.cs
@@ -6,14 +6,21 @@ public class ColorChangerScript : MonoBehaviour
 {
     private Color color;
     [SerializeField] private Material material;
+    [SerializeField] private GameObject thisColorDoors;
+    private GameObject[] colorDoors;
 
     private void Awake() {
         color = material.GetColor("_BaseColor");
+        colorDoors = GameObject.FindGameObjectsWithTag("colorDoors");
     }
 
     private void OnTriggerEnter2D(Collider2D other) {
         if(other.gameObject.tag == "Player"){
             other.GetComponent<ChangeColor>().PlayerChangeColor(color);
+            for(int i = 0; i < colorDoors.Length; i++){
+                colorDoors[i].GetComponent<Collider2D>().isTrigger = false;
+            }
+            thisColorDoors.GetComponent<Collider2D>().isTrigger = true;
         }
     }
 }
