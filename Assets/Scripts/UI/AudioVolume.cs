bd637e51e89e087ecda9f01c4d8f11558c9cc61b
Newc42 <mikolaj.cwen123@gmail.com>
2024-07-19 18:06:36 +0200

    Don't download

--- Assets/Scripts/UI/AudioVolume.cs
+++ Assets/Scripts/UI/AudioVolume.cs
@@ -2,10 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class AudioVolume : MonoBehaviour
 {
     [SerializeField] private AudioMixer audioMixer;
+    private Slider slider;
+
+    private void Awake() {
+        slider = GetComponent<Slider>();
+        float velocity;
+        audioMixer.GetFloat("volume", out velocity);    
+
+        slider.value = velocity;
+    }
 
     public void SetAudioVolume(float volume){
         if(volume <= -20){
